4a12e4f93f1509c9fbd1e288d8055fbc5968523c
Himanshu Chhabra <himichhabra14@gmail.com>
2025-10-02 22:21:40 +0530

    10. Range for looping

--- sum/sum.go
+++ sum/sum.go
@@ -2,8 +2,11 @@ package sum
 
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	// NOTE
+	// 1. range is the declarative way of looping over an array.
+	// 2. Opposite parameter convention to js, first is the index and second is the value.
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
