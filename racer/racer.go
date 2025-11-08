bccc18b211914cbbc7bc623b918096973729a73e
Himanshu Chhabra <himichhabra14@gmail.com>
2025-11-08 12:13:15 +0530

    22. Concurrency with select

--- racer/racer.go
+++ racer/racer.go
@@ -1,9 +1,12 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
+
+var tenSecondTimeout = 10 * time.Second
 
 func measureResponseTime(url string) time.Duration {
 	start := time.Now()
@@ -16,14 +19,33 @@ func measureResponseTime(url string) time.Duration {
 // therefore mock libraries for http requests are already present.
 // This is why we don't need any external dependencies and prevent
 // dep injection.
-func Racer(a, b string) (winner string) {
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 
-	aDuration := measureResponseTime(a)
+}
 
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
-		return a
+// NOTE:
+// 1. Select helps us wait on multiple channels as a way to handle concurrency.
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
-	return b
+}
+
+// NOTE:
+// 1. Never make channcels using var, since the zero
+// value is nil which will block the main thread forever if sent through
+// the channel.
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
