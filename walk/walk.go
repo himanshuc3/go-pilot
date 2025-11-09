41a1cb360492a265316760386eb7419a9d1e7f6b
Himanshu Chhabra <himichhabra14@gmail.com>
2025-11-09 11:42:18 +0530

    23. Practised Reflect and it's usage

---
+++ walk/walk.go
@@ -0,0 +1,60 @@
+package walk
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	// NOTE:
+	// 1. Heavily contrived piece of code to iterate over the fields of a struct
+	// using reflect.
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+		// for i := 0; i < val.Len(); i++ {
+		// 	// NOTE:
+		// 	// 1. Get the value at the index in slice using reflect
+		// 	// and get it's interface to be passed to the wal function.
+		// 	walk(val.Index(i).Interface(), fn)
+		// }
+	case reflect.String:
+		fn(val.String())
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
+	case reflect.Chan:
+		for {
+			if v, ok := val.Recv(); ok {
+				walkValue(v)
+			} else {
+				break
+			}
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
+	}
+
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
+}
